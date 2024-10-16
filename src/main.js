deea490ba6c6da20c84c9c1f7546bca32e90fefb
rafaelromanini <rafaromaninideoliveira@gmail.com>
2024-10-16 00:54:26 -0300

    Solução Teste Divulgador inteligente

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const { Telegraf } = require("telegraf");
 
-const token = "";
+const token = "7737288371:AAH8-cW98QNnmimQup2Ou33bpbOTDQ0z86s";
 const bot = new Telegraf(token);
 
 async function main() {
@@ -24,13 +24,28 @@ async function getLowestPrice(ctx) {
   const response = await axios.get(url);
 
   // Use o Cheerio para converter html em DOM
+  const $ = cheerio.load(response.data);
 
-  // Em seguida altere as linhas abaixo para retornar o nome e preço do plano pago mais barato
-  const lowestPlanPrice = "";
-  const lowestPlanName = "";
+  // armazenar os planos
+  const planos = []
+
+  // coleta todos os planos disponives na classe head
+  $('.col-md-4').each((index, element) => {
+    const nome = $(element).find('.head').text().trim(); // procura o nome de cada plano
+    const priceString = $(element).find('.currency').text(); // procura o preço mas no tipo string
+    const price = parseFloat(priceString.replace(/[R$.,]/g, '').trim()); // converte a string para um número
+
+    console.log(nome,priceString,price)
+
+    planos.push({ nome, price }); // adiciona o objeto a lista de planos  
+  });
+
+  const lowestPlan = planos.reduce((prev, curr) => {
+    return (prev.price < curr.price) ? prev : curr;
+  });
 
   await ctx.reply(
-    `O plano pago mais barato é o "${lowestPlanName}" e custa "${lowestPlanPrice}"`
+    `O plano pago mais barato é o "${lowestPlan.nome}" e custa "R$ ${lowestPlan.price.toFixed(2)}."`
   );
 }
 
